4bc9c4a25db29473a8886f2a1839fa998ceba0a1
Francisco Italo <italosilva1@ufrn.edu.br>
2019-12-30 19:24:41 -0300

    mobile version

---
+++ js/t.js
@@ -0,0 +1,8 @@
+var data = document.querySelector('#data');
+var dica = document.querySelector('#tip');
+
+data.onclick = function(){
+    console.log("uhu");
+    dica.style.visibility ="visible" ;
+
+}
